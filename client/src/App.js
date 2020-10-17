54699a0098f3106ab240363bb227eaefa1f884fa
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-10-17 13:41:36 +0100

    Improvement(Interface) styling and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,106 +1,52 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
+import Balance from "./components/balance.js";
+import Deposit from "./components/deposit.js";
+import Withdraw from "./components/withdraw.js";
 
 import "./App.css";
 
 class App extends Component {
   state = {
-    storageValue: "",
-    web3: null,
-    accounts: null,
-    contract: null,
-    newValue: "",
-    owner: null,
+    interface: "Balance",
   };
 
-  componentDidMount = async () => {
-    try {
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.error(error);
-    }
-  };
-  handleChange(event) {
-    this.setState({ newValue: event.target.value });
-  }
-  async handleSubmit(event) {
-    event.preventDefault();
-    const { accounts, contract } = this.state;
-    await contract.methods.set(this.state.newValue).send({ from: accounts[0] });
-    const response = await contract.methods.get().call();
-    this.setState({ storageValue: response });
-  }
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set("Changed this text").send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-
-    const response = await contract.methods.get().call();
-
-    this.setState({ storageValue: response });
-
-    // Update state with the result.
-  };
-  getOwner = async () => {
-    const { accounts, contract } = this.state;
-
-    const response2 = await contract.methods.getWhoLastChangedTheWord().call();
-
-    console.log("response2");
-    console.log(response2);
-    this.setState({ owner: response2 });
-  };
-  interFace = () => {
+  componentDidMount = async () => {};
+  card = () => {
     return (
       <div className="card">
-        <div id="header">
+        <div className="cardheader">
           <div>
-            <p>Balance</p>
+            <p>Stats</p>
           </div>
           <div>
-            <p>Supply</p>
+            <p>Deposit</p>
           </div>
           <div>
             <p>Withdraw</p>
           </div>
         </div>
-        <div></div>
+        <div>{this.whichInterface()}</div>
       </div>
     );
+  };
+  whichInterface = () => {
+    switch (this.state.interface) {
+      case "Balance":
+        return <Balance />;
+      case "Deposit":
+        return <Deposit />;
+      case "withdraw":
+        return <Withdraw />;
+    }
   };
 
   render() {
     return (
       <div className="App">
-        <h1>Poken Testing Ground</h1>
-        {this.interFace}
-        <a>Button</a>
+        <h1>Poken</h1>
+        {this.card()}
       </div>
     );
   }
---
+++ client/src/components/balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Balance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <p>Balance</p>;
+  }
+}
