0672750608d30e407b8c098f3bc16fa947937691
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-10-18 14:00:02 +0100

    improvement(randomNumber.sol)

--- interfaces/Compound.sol
+++ interfaces/Compound.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.7.0;
 
 interface Comp {
-    function mint() external payable;
+    function mint(uint256 mintAmount) external returns (uint256);
+
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
+
+    function balanceOfUnderlying(address owner) external returns (uint256);
+
+    function getCash() external view returns (uint256);
+
+    function supplyRatePerBlock() external view returns (uint256);
 }
