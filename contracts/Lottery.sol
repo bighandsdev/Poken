2f0aa112b1b9f9ef1f8d22661b1abfdd7addd91f
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-10-19 18:14:19 +0100

    improvement(Lottery.sol) added function to engage session

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,13 +1,16 @@
 pragma solidity ^0.5.0;
 
 import "./Token.sol";
+import "./DaiToken.sol";
+import "./Compound.sol";
 
 contract Lottery {
     string public name = "Poken Lottery";
     uint256 public whichSession;
     address public owner;
-    Poken public dappToken;
+    Poken public poken;
     DaiToken public daiToken;
+    Comp public compound;
 
     address[] public stakers;
     mapping(address => uint256) public stakingBalance;
@@ -53,7 +56,7 @@ contract Lottery {
         // Require amount greater than 0
         require(balance > 0, "staking balance cannot be 0");
         // checks if they are currently in the session
-        if (session[msg.sender] == whichSession) {
+        if (session[msg.sender] == !whichSession) {
             // Transfer Mock Dai tokens to this contract for staking
             daiToken.transfer(msg.sender, balance);
 
@@ -75,12 +78,47 @@ contract Lottery {
             address recipient = stakers[i];
             uint256 balance = stakingBalance[recipient];
             if (balance > 0) {
-                dappToken.transfer(recipient, balance);
+                poken.transfer(recipient, balance);
             }
         }
     }
 
-    function earn() public {
+    function engageSessionA() public {
+        require(msg.sender == owner, "caller must be the owner");
+
+        uint256 totalAmount = 0
+
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 userBalance = stakingBalance[recipient];
+            if (balance > 0) {
+                totalAmount = totalAmount + balance
+            }
+        }
+        compound.mint(totalAmount);
+    }
+
+    function engageSessionB() public {
+        require(msg.sender == owner, "caller must be the owner");
+
+        uint256 totalAmount = 0
+
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 userBalance = stakingBalance[recipient];
+            if (balance > 0) {
+                totalAmount = totalAmount + balance
+            }
+        }
+        compound.mint(totalAmount);
+    }
+
+    function disengageSessionA() public {
+        require(msg.sender == owner, "caller must be the owner");
+        
+    }
+
+    function disengageSessionB() public {
         require(msg.sender == owner, "caller must be the owner");
     }
 }
