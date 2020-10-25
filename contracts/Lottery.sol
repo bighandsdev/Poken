785982ba3e038e82e620f099662a3fbab1161463
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-10-25 16:18:56 +0000

    Progress(Lottery.sol) deposit and withdraws

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -18,57 +18,49 @@ contract Lottery {
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
 
-    constructor(Poken _dappToken, DaiToken _daiToken) public {
-        dappToken = _dappToken;
-        daiToken = _daiToken;
-        whichSession = 1;
+    constructor() public {
+        /*     whichSession = 1; */
         owner = msg.sender;
     }
 
-    function deposit(uint256 _amount) public {
-        // Require amount greater than 0
-        require(_amount > 0, "amount cannot be 0");
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-        // Trasnfer Mock Dai tokens to this contract for staking
-        daiToken.transferFrom(msg.sender, address(this), _amount);
-
+    function deposit(uint256 _amount) public payable {
         // Update staking balance
         stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
 
         // assign session
         session[msg.sender] = whichSession;
 
-        // Add user to stakers array *only* if they haven't staked already
+        /*         // Add user to stakers array *only* if they haven't staked already
         if (!hasStaked[msg.sender]) {
             stakers.push(msg.sender);
-        }
-
+        } */
+        /* 
         // Update staking status
         isStaking[msg.sender] = true;
-        hasStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true; */
     }
 
     // Unstaking Tokens (Withdraw)
-    function withdraw() public {
+    function withdraw(uint256 _amount) public {
         // Fetch staking balance
         uint256 balance = stakingBalance[msg.sender];
-
-        // Require amount greater than 0
         require(balance > 0, "staking balance cannot be 0");
-        // checks if they are currently in the session
-        if (session[msg.sender] == !whichSession) {
-            // Transfer Mock Dai tokens to this contract for staking
-            daiToken.transfer(msg.sender, balance);
 
-            // Reset staking balance
+        msg.sender.transfer(_amount);
+        /*         emit Transfer(_amount); */
+        if (balance == 0) {
             stakingBalance[msg.sender] = 0;
-
-            // Update staking status
             isStaking[msg.sender] = false;
         }
     }
 
-    // Issuing Tokens
+    function getBalance() public view returns (uint256) {
+        return stakingBalance[msg.sender];
+    }
+
+    /*     // Issuing Tokens
     function issueTokens() public {
         // Only owner can call this function
         require(msg.sender == owner, "caller must be the owner");
@@ -79,11 +71,11 @@ contract Lottery {
             uint256 balance = stakingBalance[recipient];
             if (balance > 0) {
                 poken.transfer(recipient, balance);
-            };
+            }
         }
-    }
+    } */
 
-    function engageSessionA() public {
+    /*     function engageSessionA() public {
         require(msg.sender == owner, "caller must be the owner");
 
         uint256 totalAmount = 0;
@@ -92,33 +84,33 @@ contract Lottery {
             address recipient = stakers[i];
             uint256 userBalance = stakingBalance[recipient];
             if (balance > 0) {
-                totalAmount = totalAmount + balance
-            };
-        }
-        compound.mint(totalAmount);
-    }
-
-    function engageSessionB() public {
-        require(msg.sender == owner, "caller must be the owner");
-
-        uint256 totalAmount = 0;
-
-        for (uint256 i = 0; i < stakers.length; i++) {
-            address recipient = stakers[i];
-            uint256 userBalance = stakingBalance[recipient];
-            if (balance > 0) {
-                totalAmount = totalAmount + balance
+                totalAmount = totalAmount + balance;
             }
         }
         compound.mint(totalAmount);
-    }
+    } */
 
-    function disengageSessionA() public {
+    /*     function engageSessionB() public {
+        require(msg.sender == owner, "caller must be the owner");
+
+        uint256 totalAmount = 0;
+
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint256 userBalance = stakingBalance[recipient];
+            if (balance > 0) {
+                totalAmount = totalAmount + balance;
+            }
+        }
+        compound.mint(totalAmount);
+    } */
+
+    /*     function disengageSessionA() public {
         require(msg.sender == owner, "caller must be the owner");
         
     }
 
     function disengageSessionB() public {
         require(msg.sender == owner, "caller must be the owner");
-    }
+    } */
 }
--- migrations/3_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const Lottery = artifacts.require("../contracts/Lottery.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(Lottery);
-};
